1320b499381aba31b124e44c0ef3cf13f8c8e234
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-04-28 15:45:34 -0300

    feat:18. Acción de registro de usuarios

--- Backend/src/controllers/user.js
+++ Backend/src/controllers/user.js
@@ -1,11 +1,44 @@
 'use strict';
 
+const bcrypt = require('bcrypt-nodejs');
+const User = require('./../models/user');
+
 function pruebas(req, res){
   res.status(200).send({
     message: "Probando una acción del controllador de Usuarios del API-REST"
   });
 };
 
+function saveUser(req, res){
+  const user = new User();
+  const params = req.body;
+
+  user.name = params.name;
+  user.surname = params.surname;
+  user.email = params.email;
+  user.role = "ROLE_USER";
+
+  if(!user.name || !user.surname || !user.email){
+    res.status(400).send({ message: "Rellena todos los campos" });
+  };
+
+  if(params.password){
+    // Encriptar contraseña y guardar datos
+    bcrypt.hash(params.password, null, null, function(err, hash){
+      user.password = hash;
+
+      user.save(function(err, userStored){
+        if(err) res.status(500).send({ message: "Error al guardar el usuario" });
+        else if(!userStored) res.status(400).send({ message: "No se ha registrado el usuario" });
+        else res.status(200).send({ user: userStored });
+      });
+    });
+  } else{
+    res.status(400).send({ message: "Introduce una contraseña" });
+  };
+};
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -8,5 +8,6 @@ const UserCtrl = require("./../controllers/user");
 const API = express.Router();
 
 API.get('/test', UserCtrl.pruebas);
+API.post('/register', UserCtrl.saveUser);
 
 module.exports = API;
