10a06cafbca68f170a4b73cd287cc20d77ab8da8
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-06 18:12:18 -0300

    build: add configFiles

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('dotenv').config();
+
+
+const colors = require('colors');
+
+const APP = require('./server/express');
+const MONGO = require('./server/mongo');
+
+const PORT =  process.env.PORT || 3000;
+
+
+// Inicialización del servidor Express
+APP.listen(PORT, ()=>{
+  let url = `${colors.grey('http://localhost:')}${colors.blue(PORT)}`;
+  console.log(`Servidor ${colors.green('ON')}: ${url}`);
+
+}).on('error', (error) => {
+  console.log(`${colors.red('Ocurrio un Error: ')}`, error.message);
+});
+
+// Inicialización a la conexión MongoDB
+MONGO.then(()=>{
+  let MongoDB = `${colors.grey('Mongo')}${colors.brightGreen('DB')}`;
+  console.log(`${MongoDB}: ${colors.green('Conectado')}`);
+}).catch((error)=>{
+  console.log(error);
+});
