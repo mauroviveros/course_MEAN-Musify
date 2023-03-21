507ed7509535b5f16d30ab40c368b6dadb0f3bff
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-20 23:10:13 -0300

    feat(course): :white_check_mark: 26. Crear un artista

--- server/controllers/artist.js
+++ server/controllers/artist.js
@@ -12,6 +12,21 @@ async function getArtist(req, res){
   return res.status(200).send({ message: "Metodo getArtist" });
 };
 
+async function uploadArtist(req, res){
+  const params  = req.body;
+  const artist    = new Artist(params);
+  artist.image  = undefined;
+
+  try {
+    const artistStored = await artist.save();
+    return res.send({ artist: artistStored });
+  } catch (error) {
+    res.status(error.status || 400).json(error);
+  }
+
+}
+
 module.exports = {
-  getArtist
+  getArtist,
+  uploadArtist
 }
--- server/models/artist.js
+++ server/models/artist.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ArtistSchema = Schema({
-  name: String,
-  description: String,
+  name: { type: String, required: [true, "Rellena todos los campos"]},
+  description: { type: String, required: [true, "Rellena todos los campos"] },
   image: String
 });
 
