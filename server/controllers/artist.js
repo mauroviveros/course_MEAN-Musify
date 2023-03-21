57bd326598116345d7029891d48b7352e1119da7
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-21 00:20:50 -0300

    feat(course): :white_check_mark: 31. Métodos las imágenes del artista

--- server/controllers/artist.js
+++ server/controllers/artist.js
@@ -14,7 +14,7 @@ async function getArtist(req, res){
     const artistDB = await Artist.findById(req.params._id);
     return res.json(artistDB);
   } catch (error) {
-    return res.status(error.status || 400).json(error);
+    return res.status(400).json({ message: "Error al obtener el detalle del artista", error: { message: error.message } });
   };
 };
 
@@ -26,7 +26,7 @@ async function getArtists(req, res){
     const artistsDB = await Artist.paginate({}, { page, limit });
     return res.json(artistsDB);
   } catch (error) {
-    return res.status(error.status || 400).json(error);
+    return res.status(400).json({ message: "Error al obtener el listado de artistas", error: { message: error.message } });
   }
 }
 
@@ -38,7 +38,7 @@ async function uploadArtist(req, res){
     const artistStored = await artist.save();
     return res.json(artistStored);
   } catch (error) {
-    return res.status(error.status || 400).json(error);
+    return res.status(400).json({ message: "Error al guardar el artista", error: { message: error.message } });
   };
 };
 
@@ -48,28 +48,66 @@ async function updateArtist(req, res){
     const artistUpdated = await Artist.findByIdAndUpdate(req.params._id, req.body, { new: true });
     return res.json(artistUpdated);
   } catch (error) {
-    return res.status(error.status || 400).json(error);
+    return res.status(400).json({ message: "Error al actualizar el artista", error: { message: error.message } });
   };
 };
 
 async function deleteArtist(req, res){
   try {
     const artistDeleted = await Artist.findByIdAndDelete(req.params._id);
-    if(!artistDeleted) throw { status: 400, message: "No existe el artista que desea eliminar" };
+    if(!artistDeleted) throw new Error("No existe el artista que desea eliminar");
 
     const albumDeleted = await Album.findOneAndDelete({ artist: artistDeleted._id });
     if(albumDeleted) await Song.findOneAndDelete({ album: albumDeleted._id });
 
     return res.json({ status: 200, artist: artistDeleted });
   } catch (error) {
-    return res.status(error.status || 400).json(error);
+    return res.status(400).json({ message: "Error al borrar el artista", error: { message: error.message } });
   }
 };
+
+
+async function uploadImage(req, res){
+  let file_name, file_ext;
+
+  try {
+    if(req.files){
+      file_name = path.basename(req.files.image.path);
+      file_ext  = path.extname(file_name);
+    } else throw new Error("No has subido ninguna imagen...");
+
+  
+
+    if(file_ext === ".png" || file_ext === ".jpg" || file_ext === ".gif"){
+      const artistUpdated = await Artist.findByIdAndUpdate(req.params._id, { image: file_name }, { new: true });
+      return res.send(artistUpdated);
+    } else throw new Error("Extension del archivo no valida");
+  } catch(error){
+    return res.status(400).json({ message: "Error al actualizar la imagen del artista", error: { message: error.message } });
+  };
+};
+
+async function getImage(req, res){
+  try{
+    const artistDB = await Artist.findById(req.params._id);
+    if(!artistDB) throw new Error("No existe el usuario");
+    const path_file = `./uploads/artists/${artistDB.image}`;
+    const imageBool = await fs.existsSync(path_file);
+
+    if(!imageBool) throw new Error("No existe la imagen");
+
+    res.sendFile(path.resolve(path_file));
+  } catch(error){
+    return res.status(500).send({ message: "Error al obtener la imagen del usuario", error: { message: error.message } });
+  }
+}
 
 module.exports = {
   getArtist,
   getArtists,
   uploadArtist,
   updateArtist,
-  deleteArtist
+  deleteArtist,
+  uploadImage,
+  getImage
 };
--- server/routes/artist.js
+++ server/routes/artist.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const express = require("express");
+const multipart = require("connect-multiparty");
 
 const ArtistCtrl = require("../controllers/artist");
 const auth_md = require("../middlewares/authenticated");
+const upload_md = multipart({ uploadDir: "./uploads/artists" });
 
 
 const API = express.Router();
@@ -16,5 +18,8 @@ API.post("/artists", auth_md.ensureAuth, ArtistCtrl.uploadArtist);
 API.put("/artists/:_id", auth_md.ensureAuth, ArtistCtrl.updateArtist);
 
 API.delete("/artists/:_id", auth_md.ensureAuth, ArtistCtrl.deleteArtist);
+
+API.get("/artists/:_id/image", ArtistCtrl.getImage);
+API.post("/artists/:_id/image", [auth_md.ensureAuth, upload_md], ArtistCtrl.uploadImage);
 
 module.exports = API;
