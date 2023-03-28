b8be888ecc20d3fa6b0854f4c914642bbbb9a91b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-28 13:47:48 -0300

    feat(course): :white_check_mark: 41. Conseguir una canción

--- server/controllers/song.js
+++ server/controllers/song.js
@@ -6,7 +6,12 @@ const fs    = require("fs");
 const Song  = require("../models/song");
 
 async function getSong(req, res){
-  res.send({ message: "Controllador de canción" });
+  try {
+    const songDB = await Song.findById(req.params._id).populate({ path: "album", populate: { path: "artist" } }).exec();
+    return res.json(songDB);
+  } catch (error) {
+    return res.status(400).json({ message: "Error al obtener el detalle de la canción", error: { message: error.message } });
+  };
 };
 
 async function uploadSong(req, res){
