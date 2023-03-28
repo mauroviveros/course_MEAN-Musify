a359e781740f26a6687cfef3c1e878f3d403ca30
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-28 14:28:53 -0300

    feat(course): :white_check_mark: 46. Configurar cabeceras (CORS)

--- server/express.js
+++ server/express.js
@@ -13,13 +13,13 @@ APP.use(bodyParser.urlencoded({ extended: false }))
 APP.use(bodyParser.json())
 
 // Configurar Cabeceras HTTP
-// APP.use(function (req, res, next){
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
+APP.use(function (req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 // Cargas de Rutas Base
 APP.use("/api", user_routes);
