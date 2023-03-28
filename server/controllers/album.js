8bc9b36863b4029503574a38a16efe8cc2303657
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-28 12:27:23 -0300

    feat(course): :white_check_mark: 34. Devolver un album

--- server/controllers/album.js
+++ server/controllers/album.js
@@ -4,7 +4,7 @@ const Album   = require("../models/album");
 
 async function getAlbum(req, res){
   try {
-    const albumDB = await Album.findById(req.params._id);
+    const albumDB = await Album.findById(req.params._id).populate({ path: "artist" }).exec();
     return res.json(albumDB);
   } catch (error) {
     return res.status(400).json({ message: "Error al obtener el detalle del album", error: { message: error.message } });
