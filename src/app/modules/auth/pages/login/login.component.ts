5333ebfac285f4bfa15b48240ee9b711f6a01678
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-02 16:54:05 -0300

    feat(course): :white_check_mark: 55. Two-way data binding en formularios

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent {
+  public rememberControl: FormControl = new FormControl(false);
 
-}
+  public form: FormGroup = this._formBuilder.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required]]
+  });
+
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ){}
+
+
+  public validate(controlName: string){
+    const formControl = this.form.get(controlName);
+    const isValid = formControl?.valid;
+    if(formControl?.dirty || formControl?.touched) return isValid ? "is-valid" : "is-invalid";
+    return;
+  };
+
+  public submit(){
+    if(this.form.invalid) return Object.values(this.form.controls).forEach(control => control.markAsDirty());
+
+    console.log(this.form.value);
+    console.log(this.rememberControl.value);
+  }
+
+};
--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent {
+  public form: FormGroup = this._formBuilder.group({
+    name:     [null, [Validators.required]],
+    surname:  [null, [Validators.required]],
+    email:    [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.minLength(6)]]
+  });
 
-}
+  constructor(
+    private _formBuilder: FormBuilder
+  ){};
+
+  public validate(controlName: string){
+    const formControl = this.form.get(controlName);
+    const isValid = formControl?.valid;
+    if((formControl?.dirty || formControl?.touched)) return isValid ? "is-valid" : "is-invalid";
+    return;
+  };
+
+  public submit(){
+    if(this.form.invalid) return Object.values(this.form.controls).forEach(control => control.markAsDirty());
+
+
+    console.log(this.form.value);
+  }
+};
