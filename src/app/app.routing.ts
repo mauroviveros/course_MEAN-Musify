2578e9d1fb357deedaf493cc4308b7a5edb7fbcb
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-02 11:48:27 -0300

    feat(course): :white_check_mark: 53. Componente principal y vistas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", loadChildren: ()=> import("./modules/musify/musify.module").then(m => m.MusifyModule) },
+  { path: "", loadChildren: ()=> import("./modules/auth/auth.module").then(m => m.AuthModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/musify/musify.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MusifyRoutingModule } from './musify.routing';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    MusifyRoutingModule
+  ]
+})
+export class MusifyModule { }
