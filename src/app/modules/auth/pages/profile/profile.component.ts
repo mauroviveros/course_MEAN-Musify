9caf823ab6174c58ec7a3a3710701c553dc97097
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-02 22:23:26 -0300

    feat(course): :white_check_mark: 71. Actualizar los datos usuario

--- src/app/modules/auth/pages/profile/profile.component.ts
+++ src/app/modules/auth/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import Swal from 'sweetalert2';
 import { AuthService } from '../../auth.service';
 @Component({
   selector: 'app-profile',
@@ -9,31 +10,46 @@ import { AuthService } from '../../auth.service';
 export class ProfileComponent {
   @ViewChild("inputFile") inputFile: ElementRef = {} as ElementRef;
 
+  public imageSrc: string | null = null;
+
   public get imgProfileURL (){
-    return this._auth.imgProfileURL();
+    return this.imageSrc || this._auth.imgProfileURL();
   }
 
   public form: FormGroup = this._formBuilder.group({
-    name: [null],
-    surname: [null],
-    email: [null]
+    name: [this._auth.user.name, [Validators.required]],
+    surname: [this._auth.user.surname, [Validators.required]],
+    email: [{ value: this._auth.user.email, disabled: true }]
   });
 
   constructor(
     private _formBuilder: FormBuilder,
     private _auth: AuthService
-  ){}
+  ){};
 
-  ngAfterViewInit(){
-    const user = this._auth.user;
-    this.form.get("name")?.setValue(user.name);
-    this.form.get("surname")?.setValue(user.surname);
-    this.form.get("email")?.setValue(user.email);
-
-  }
-
-
-  test(){
+  openFile(){
     this.inputFile.nativeElement.click();
+  };
+
+  onFileSelected(event:any){
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => { this.imageSrc = e.target.result; };
+    reader.readAsDataURL(file);
+  };
+
+
+  submit(){
+    if(this.form.invalid) return;
+
+    const body = this.form.value;
+    this._auth.updateUser(body).subscribe(response => {
+      Swal.fire({
+        title: "Detalle del Usuario",
+        text: "Usuario Actualizado correctamente",
+        icon: "success"
+      })
+    })
   }
 }
