d5ce548e99526c14fe7da1b815427c591bf9d21f
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-17 22:30:46 -0300

    feat(course): :white_check_mark: 105. Componente para el detalle del album

--- src/app/modules/album/pages/detail/detail.component.ts
+++ src/app/modules/album/pages/detail/detail.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Album } from '../../album.interface';
+import { AlbumService } from '../../album.service';
+import Swal from 'sweetalert2';
+import { finalize } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-detail',
+  selector: 'album-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent {
+  public album?: Album;
 
+  public get img(){
+    return (this.album?._id ? this.albumService.getImg(this.album._id) : null)
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private albumService: AlbumService
+  ){
+    Swal.fire("obteniendo Album", undefined, "info");
+    Swal.showLoading();
+
+    this.albumService.get(this.route.snapshot.paramMap.get("_id") as string).pipe(
+      finalize(() => Swal.close())
+    ).subscribe({
+      next: response => { this.album = response; },
+      error: () => { this.router.navigate(["/"]); }
+    });
+  }
 }
