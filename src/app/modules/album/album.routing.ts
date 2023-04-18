75c4a3995f801060ffcada6429a9b2d7a7a3fdcc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-17 22:31:10 -0300

    feat(course): :white_check_mark: 105. Componente para el detalle del album

--- src/app/modules/album/album.routing.ts
+++ src/app/modules/album/album.routing.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CreateComponent } from './pages/create/create.component';
 import { UpdateComponent } from './pages/update/update.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 const routes: Routes = [
   // { path: "", component: ListComponent },
   { path: "create", component: CreateComponent },
-  // { path: ":_id", component: DetailComponent },
+  { path: ":_id", component: DetailComponent },
   { path: ":_id/update", component: UpdateComponent }
 ];
 
