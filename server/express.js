31adf2c2e8ca5bdc377fd42edac0b764eed8aedb
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-13 16:17:23 -0300

    feat: :alien: add public path on express

--- server/express.js
+++ server/express.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors    = require("cors");
 const bodyParser = require("body-parser");
+const path = require('path');
 const APP = express();
 
 const user_routes   = require("./routes/user");
@@ -16,14 +17,21 @@ APP.use(bodyParser.json())
 // Configurar Cabeceras HTTP
 APP.use(cors());
 
+
 // Cargas de Rutas Base
 APP.use("/api", user_routes);
 APP.use("/api", artist_routes);
 APP.use("/api", album_routes);
 APP.use("/api", song_routes);
 
-APP.get("/pruebas", function(req, res){
-    res.status(200).send({ message: "Bienvenido a Musify" });
+APP.get("/version", function(req, res){
+  const { name, description, author, version } = require("../package.json");
+
+  res.status(200).send({ name, description, author, version });
 });
+
+APP.use(express.static(path.join(__dirname, "../public")));
+APP.use("*", (req, res)=>{ res.sendFile(path.join(__dirname, "../public/index.html")); });
+
 
 module.exports = APP;
