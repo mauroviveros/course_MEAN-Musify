d2fa1f94ba49f70a70728bbb537ddd1f012f1630
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-18 23:44:52 -0300

    feat(frontend.shared): :arrow_up: import modules
    modules on sharedModule and materialModule

--- frontend/src/app/shared/modules/material/material.module.ts
+++ frontend/src/app/shared/modules/material/material.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
 
-@NgModule({})
+@NgModule({
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+  ],
+})
 export class MaterialModule {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  exports: [MaterialModule],
+  exports: [CommonModule, ReactiveFormsModule, MaterialModule],
 })
 export class SharedModule {}
