31245490d5390e567f1181f5c88ba6ffd3d8cfab
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-19 22:40:09 -0300

    feat(frontend.shared): :arrow_up: import materialModules
    add MatButtonModule

--- frontend/src/app/shared/modules/material/material.module.ts
+++ frontend/src/app/shared/modules/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,6 +9,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   exports: [
+    MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
