e18fe44fbb91743048e61849b42e361f93011ccc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-24 16:54:59 -0300

    feat(frontend.auth): :sparkles: add maxLength on inputs
    in the name and surname inputs

--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -7,10 +7,11 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   private readonly fb = inject(FormBuilder);
+  readonly MAX_LENGTH = 24;
 
   readonly form = this.fb.group({
-    name: ['', [Validators.required]],
-    surname: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
+    surname: ['', [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
