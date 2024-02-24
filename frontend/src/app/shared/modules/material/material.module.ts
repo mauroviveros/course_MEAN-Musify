f0c01925aed18b14dc3eb10cf8ab46c6c91f85f3
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-24 18:11:41 -0300

    feat(frontend.shared): :heavy_plus_sign: import materialModules
    add MatCheckboxModule

--- frontend/src/app/shared/modules/material/material.module.ts
+++ frontend/src/app/shared/modules/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -11,6 +12,7 @@ import { MatTabsModule } from '@angular/material/tabs';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
