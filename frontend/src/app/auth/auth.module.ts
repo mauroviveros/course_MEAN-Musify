6d120a05502d380bcced0afa52b943ce34212f6f
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-24 18:10:42 -0300

    feat(frontend.auth): :sparkles: add remember and terms caption

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [SharedModule],
--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent {
   private readonly fb = inject(FormBuilder);
 
+  readonly remember = this.fb.control(false);
   readonly form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
