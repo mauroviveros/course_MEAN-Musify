07a1edef600c0c5b8d581adbbcfd0ade2bd30eba
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-25 15:38:01 -0300

    feat(frontend.shared): :heavy_plus_sign: import materialModules
    add MatProgressBarModule

--- frontend/src/app/shared/modules/material/material.module.ts
+++ frontend/src/app/shared/modules/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatProgressBarModule,
     MatTabsModule,
   ],
 })
