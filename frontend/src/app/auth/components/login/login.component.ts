e8902590e8a0ee9904139bf1df8275d4ba8c95d4
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-26 11:32:01 -0300

    feat(frontend.auth): :construction: WIP submit on auth Forms

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+import { MessageService } from '../../../shared/services/message.service';
+
 @Component({
   selector: 'auth-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  @Output() loading = new EventEmitter<boolean>();
   private readonly fb = inject(FormBuilder);
+  private readonly message = inject(MessageService);
+
+  @Output() loading = new EventEmitter<boolean>();
 
   readonly remember = this.fb.control(false);
   readonly form = this.fb.group({
@@ -19,6 +23,10 @@ export class LoginComponent {
     if (this.form.invalid) return;
     this.loading.emit(true);
 
-    // console.log(this.form.value);
+    setTimeout(() => {
+      this.message.success('Sesión iniciada correctamente');
+      this.loading.emit(false);
+      console.log(this.form.value);
+    }, 1000);
   }
 }
--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -1,15 +1,19 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+import { MessageService } from '../../../shared/services/message.service';
+
 @Component({
   selector: 'auth-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-  @Output() loading = new EventEmitter<boolean>();
   private readonly fb = inject(FormBuilder);
-  readonly MAX_LENGTH = 24;
+  private readonly message = inject(MessageService);
 
+  @Output() loading = new EventEmitter<boolean>();
+
+  readonly MAX_LENGTH = 24;
   readonly form = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
     surname: ['', [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
@@ -21,6 +25,10 @@ export class RegisterComponent {
     if (this.form.invalid) return;
     this.loading.emit(true);
 
-    // console.log(this.form.value);
+    setTimeout(() => {
+      this.message.success('Usuario generado correctamente');
+      this.loading.emit(false);
+      console.log(this.form.value);
+    }, 1000);
   }
 }
