50795b26cbf03f76ea899597a24659d3fc974ff1
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-03-01 22:27:15 -0300

    feat(frontend): :sparkles: init coreComponent

--- frontend/src/app/core/core.component.ts
+++ frontend/src/app/core/core.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatDrawerMode } from '@angular/material/sidenav';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
 
+import { SharedModule } from '@shared/shared.module';
+import { CoreModule } from './core.module';
 @Component({
   selector: 'app-core',
   standalone: true,
-  imports: [],
+  imports: [CoreModule, SharedModule],
   templateUrl: './core.component.html',
   styleUrl: './core.component.scss',
 })
-export class CoreComponent {}
+export class CoreComponent {
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  readonly mode = signal<MatDrawerMode>('over');
+
+  constructor() {
+    this.breakpointObserver
+      .observe(Breakpoints.XSmall)
+      .pipe(
+        takeUntilDestroyed(),
+        map(result => (result.matches ? 'push' : 'side'))
+      )
+      .subscribe(mode => this.mode.set(mode));
+  }
+}
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@shared/shared.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   imports: [SharedModule, AuthModule],
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
 })
 export class CoreModule {}
