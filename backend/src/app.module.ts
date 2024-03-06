99cd2f2c170907e4c67f5e5105557f287bd2a284
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-03-05 21:31:28 -0300

    revert(backend): :heavy_minus_sign: remove GraphQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
-
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
-    AuthModule,
-    UsersModule,
   ],
 })
 export class AppModule {}
--- backend/src/modules/users/dto/update-user.input.ts
+++
@@ -1,8 +0,0 @@
-import { UsersCreateInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateUserInput extends PartialType(UsersCreateInput) {
-  @Field(() => Int)
-  id: number;
-}
