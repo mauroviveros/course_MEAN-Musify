3e1065d54108f3df4146eaca0602ccd14cd94a9e
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-03-16 17:04:15 -0300

    fix(backend): :bug: fix user findOne
    when user not found throw 404 error

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -9,7 +9,9 @@ export class UserService {
   constructor(@InjectModel(User.name) private model: Model<User>) {}
 
   async findOne(email: string): Promise<User> {
-    return await this.model.findOne({ email });
+    const user = await this.model.findOne({ email });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   async create(body: User): Promise<User> {
