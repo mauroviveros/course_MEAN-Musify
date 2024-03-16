84a068455b730f9995176dcbeedec74b50665e35
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-03-16 17:04:39 -0300

    feat(backend): :bricks: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { MongoErrorFilter } from '@filter/mongo-error.filter';
 import { MongooseErrorFilter } from '@filter/mongoose-error.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('/api');
   app.useGlobalFilters(new MongooseErrorFilter(), new MongoErrorFilter());
   await app.listen(3000);
