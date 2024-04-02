6d39e9b4880647604cb4434f3140be95d0630e69
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-04-02 00:14:38 -0300

    feat(frontend): :building_construction: move coreComponent to appComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatDrawerMode } from '@angular/material/sidenav';
+import { map } from 'rxjs';
+
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CoreModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'musify';
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  readonly router = inject(Router);
+  readonly mode = signal<MatDrawerMode>('over');
+
+  constructor() {
+    this.breakpointObserver
+      .observe(Breakpoints.XSmall)
+      .pipe(
+        takeUntilDestroyed(),
+        map(result => (result.matches ? 'push' : 'side'))
+      )
+      .subscribe(mode => this.mode.set(mode));
+  }
 }
--- frontend/src/app/core/core.component.ts
+++ frontend/src/app/core/core.component.ts
@@ -1,29 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { MatDrawerMode } from '@angular/material/sidenav';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs';
+import { Component } from '@angular/core';
 
-import { SharedModule } from '@shared/shared.module';
-import { CoreModule } from './core.module';
 @Component({
   selector: 'app-core',
   standalone: true,
-  imports: [CoreModule, SharedModule],
+  imports: [],
   templateUrl: './core.component.html',
   styleUrl: './core.component.scss',
 })
-export class CoreComponent {
-  private readonly breakpointObserver = inject(BreakpointObserver);
-  readonly mode = signal<MatDrawerMode>('over');
-
-  constructor() {
-    this.breakpointObserver
-      .observe(Breakpoints.XSmall)
-      .pipe(
-        takeUntilDestroyed(),
-        map(result => (result.matches ? 'push' : 'side'))
-      )
-      .subscribe(mode => this.mode.set(mode));
-  }
-}
+export class CoreComponent {}
