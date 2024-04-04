0b2cd06356bcac7023b87e03cccbceb1132d2f58
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-04-03 21:25:51 -0300

    feat(frontend): :wheelchair: add dynamic nav routes

--- frontend/src/app/core/components/menu/menu.component.ts
+++ frontend/src/app/core/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '@auth/services/auth.service';
 
 @Component({
@@ -8,6 +9,17 @@ import { AuthService } from '@auth/services/auth.service';
 })
 export class MenuComponent {
   private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  readonly routes = this.router.config
+    .filter(({ data }) => data && data['menu'])
+    .map(({ path, title, data }) => {
+      return {
+        path: path,
+        title: title,
+        icon: data?.['menu'].icon || 'dashboard',
+      };
+    });
 
   logout(): void {
     //TODO add loader on logout
